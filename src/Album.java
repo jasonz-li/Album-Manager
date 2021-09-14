d406525e4a980fbf7615a1c3a100585b2e4d5af7
jasonz-li <jasonli.0820@yahoo.com>
2021-09-14 16:38:35 -0400

    Changes by JohnLeng

--- src/Album.java
+++ src/Album.java
@@ -1,2 +1,18 @@
 public class Album {
+    private String title;
+    private String artist;
+    private Genre genre; //enum class; Classical, Country, Jazz, Pop, Unknown
+    private Date releaseDate;
+    private boolean isAvailable;
+
+
+    //John
+    @Override
+    public boolean equals(Object obj) { }
+
+
+
+    //Jason
+    @Override
+    public String toString() { }
 }
--- src/Collection.java
+++ src/Collection.java
@@ -6,10 +6,16 @@ public class Collection {
     //Jason
     private int find(Album album) {} //find the album index, or return NOT_FOUND
     private void grow() {} //increase the capacity of the array list by 4
+
+    //John
     public boolean add(Album album) {}
     public boolean remove(Album album) {}
+
+    //Jason
     public boolean lendingOut(Album album) {} //set to not available
     public boolean returnAlbum(Album album) {} //set to available
+
+    //John
     public void print() {} //display the list without specifying the order
     public void printByReleaseDate() {}
     public void printByGenre() {}
