69cb9cfc4c918c0320b5a4bb9841b52cbd0c460a
jasonz-li <jasonli.0820@yahoo.com>
2021-09-23 16:50:57 -0400

    added all my files

--- src/Collection.java
+++ src/Collection.java
@@ -4,16 +4,53 @@ public class Collection {
     private int numAlbums; //number of albums currently in the collection
 
     //Jason
-    private int find(Album album) {} //find the album index, or return NOT_FOUND
-    private void grow() {} //increase the capacity of the array list by 4
+
+    //find the album index, or return NOT_FOUND
+    private int find(Album album) {
+        for (int i = 0; i < this.numAlbums; i++) {
+            if (this.albums[i] == album) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
+    }
+
+    //increase the capacity of the array list by 4
+    private void grow() {
+        Album[] newCollection = new Album[this.numAlbums + 4];
+        for (int i = 0; i < this.numAlbums; i++){
+            newCollection[i] = this.albums[i];
+        }
+        this.albums = newCollection;
+    }
 
     //John
     public boolean add(Album album) {}
     public boolean remove(Album album) {}
 
     //Jason
-    public boolean lendingOut(Album album) {} //set to not available
-    public boolean returnAlbum(Album album) {} //set to available
+
+    // set to not available
+    public boolean lendingOut(Album album) {
+        for (int i = 0; i < this.numAlbums; i++){
+            if (this.albums[i] == album){
+                this.albums[i].isAvailable = false;
+                break;
+            }
+        }
+        return;
+    }
+
+    //set to available
+    public boolean returnAlbum(Album album) {
+        for (int i = 0; i < this.numAlbums; i++){
+            if (this.albums[i] == album){
+                this.albums[i].isAvailable = true;
+                break;
+            }
+        }
+        return;
+    }
 
     //John
     public void print() {} //display the list without specifying the order
