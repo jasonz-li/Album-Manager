a884e740d0d950e5476a3a8cd941b6ba7702c5e5
jasonz-li <jasonli.0820@yahoo.com>
2021-09-24 15:10:49 -0400

    scanner

--- src/Collection.java
+++ src/Collection.java
@@ -40,7 +40,7 @@ public class Collection {
      * @return
      */
     public boolean add(Album album) {
-        if(numALbums < albums.length) {
+        if(numAlbums < albums.length) {
             this.album[numAlbums] = album;
             this.numAlbums++;
             return true;
@@ -68,7 +68,7 @@ public class Collection {
     // set to not available
     public boolean lendingOut(Album album) {
         for (int i = 0; i < this.numAlbums; i++){
-            if (this.albums[i] == album){
+            if (this.albums[i].equals(album)){
                 this.albums[i].isAvailable = false;
                 break;
             }
@@ -79,7 +79,7 @@ public class Collection {
     //set to available
     public boolean returnAlbum(Album album) {
         for (int i = 0; i < this.numAlbums; i++){
-            if (this.albums[i] == album){
+            if (this.albums[i].equals(album)){
                 this.albums[i].isAvailable = true;
                 break;
             }
@@ -111,10 +111,10 @@ public class Collection {
 
         for (int i = 0; i < albumsLength-1; i++)
         {
-            int min_idex = i;
+            int min_idx = i;
             for (int j = i + 1; j < albumsLength; j++)
                 if (albums[i]releaseDate.compareTo(albums[j].releaseDate) == true){
-                    min_idex = j;
+                    min_idx = j;
                 }
 
             int tempVar = albums[min_idx];
@@ -133,7 +133,7 @@ public class Collection {
 
         for (int i = 0; i < albumsLength-1; i++)
         {
-            int min_idex = i;
+            int min_idx = i;
             for (int j = i + 1; j < albumsLength; j++)
                 if (albums[i].genre.toString().compareTo(albums[j].genre.toString()) >= 0){
                     min_idex = j;
--- src/CollectionManager.java
+++ src/CollectionManager.java
@@ -1,5 +1,40 @@
 public class CollectionManager {
     public void run(){
-        //????
+        public static void main(String args[]){
+            Scanner scanner = new Scanner(System.in).useDelimiter("\\s*,\\s*");
+
+            while (true){
+                if (args[0] == "A"){
+
+                }
+                else if (args[0] == "D"){
+
+                }
+                else if (args[0] == "L"){
+
+                }
+                else if (args[0] == "R") {
+
+                }
+                else if (args[0] == "P") {
+
+                }
+                else if (args[0] == "Q") {
+                    break;
+                }
+                else{
+                    invalid command
+                }
+
+                //A ADD
+                //D REMOVE
+                //L LEND
+
+                //R RETURN
+                //P DISPLAY LIST
+                //Q STOP PROGRAM
+            }
+
+        }
     }
 }
